48e553e8d838353a242bb5e00c8d046f6d703a4d
Sam Shih <samshih@217-77-24-172-wifi.hq.squareup.com>
2015-03-16 17:44:43 -0700

    git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,11 +8,12 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias ga='git add'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
+#alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
