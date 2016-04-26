e035981a372f79189f36a6658a3c8fded11157d2
Samuel Shih <sam00429@gmail.com>
2016-04-26 13:28:16 -0400

    virtual env aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias vu="vagrant up"
 alias vssh="vagrant ssh"
 alias vs="vagrant suspend"
 alias swat="ssh -Y sshih1@lab.cs.swarthmore.edu"
+alias startpy="source venv/bin/activate"
+alias killpy="deactivate"
+
